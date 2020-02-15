c92cc3e6bf456cf6aac24fe59de692d0e32e334f
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2020-02-15 16:25:44 -0600

    V2

--- routes/route.js
+++ routes/route.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/users');
+
 //retrieving data
 router.get('/users', (req, res, next)=>{
-    res.send('Retrieving the user list');
+    User.find(function(err, users){
+        res.json(users);
+    })
 });
 
 // add user
-router.post('/contact', (req, res, next)=>{
-    //logic to add contact
+router.post('/users', (req, res, next)=>{
+    //logic to add user
+    let newUser = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    PNR: req.body.PNR
+
+    });
+
+    newUser.save((err, user)=>{
+        if(err)
+        {
+            res.json({msg: 'Failed to add user'});
+        }
+        else{
+            res.json({msg: 'USer added successfully'});
+        }
+    });
 });
 
 //deleting user
-router.delete('/contact/:id', (req, res, next)=>{
-    //logic to remove contact
+router.delete('/users/:id', (req, res, next)=>{
+    //logic to remove user
+    User.remove({_id: req.params.id}), function(err, result){
+        if(err)
+        {
+            res.json(err);
+        }
+        else{
+            res.json(result);
+        }
+    }
 });
 
 module.exports = router;
